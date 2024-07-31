1bd5d74dc7cdd234ee2ca19bc6cc94241e744149
Matt Dunn <mattdunn0816@gmail.com>
2024-07-31 14:07:21 -0600

    fix: updated icons to material for consistency

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-import FontAwesome from '@expo/vector-icons/FontAwesome'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 import { Tabs } from 'expo-router'
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name']
+  name: React.ComponentProps<typeof MaterialIcons>['name']
   color: string
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />
+  return <MaterialIcons size={28} style={{ marginBottom: -3 }} {...props} />
 }
 
 export default function TabLayout() {
@@ -18,21 +18,23 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Feed',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="chat" color={color} />,
         }}
       />
       <Tabs.Screen
         name="showcase"
         options={{
           title: 'Showcase',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="style" color={color} />,
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
           title: 'Settings',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="settings" color={color} />
+          ),
         }}
       />
     </Tabs>
