3d6f3593702d10ab284df86a87b47914942617a2
Matt Dunn <mattdunn0816@gmail.com>
2024-07-31 13:55:52 -0600

    feat: setup basic tab pages

---
+++ src/app/_layout.tsx
@@ -0,0 +1,22 @@
+import { Stack } from 'expo-router'
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router'
+
+export const unstable_settings = {
+  initialRouteName: '(tabs)',
+}
+
+export default function RootLayout() {
+  return <RootLayoutNav />
+}
+
+function RootLayoutNav() {
+  return (
+    <Stack>
+      <Stack.Screen name="(home)" options={{ headerShown: false }} />
+    </Stack>
+  )
+}
