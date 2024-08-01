b68bc1f02b6c0ead0f2f4cb5ddac776b9d0e411d
Matt Dunn <mattdunn0816@gmail.com>
2024-08-01 10:01:32 -0600

    fix: eslint issues

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,15 +1,6 @@
 import React from 'react'
 
-import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 import { Tabs } from 'expo-router'
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof MaterialIcons>['name']
-  color: string
-}) {
-  return <MaterialIcons size={28} style={{ marginBottom: -3 }} {...props} />
-}
 
 export default function TabLayout() {
   return (
@@ -18,23 +9,18 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Feed',
-          tabBarIcon: ({ color }) => <TabBarIcon name="chat" color={color} />,
         }}
       />
       <Tabs.Screen
         name="showcase"
         options={{
           title: 'Showcase',
-          tabBarIcon: ({ color }) => <TabBarIcon name="style" color={color} />,
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
           title: 'Settings',
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="settings" color={color} />
-          ),
         }}
       />
     </Tabs>
